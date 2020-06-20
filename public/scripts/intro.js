ae0a35a6c925dddbd8f684988754659c59ddb6ce
Giovanne Tarcitano <44866256+Gitarcitano@users.noreply.github.com>
2020-06-20 00:09:45 -0300

    initial commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,68 @@
+// // comments
+// // document.write("Hello")
+
+// //  variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// //  string
+// const s1 = "Isso e um string"
+// const s2 = "Isso tambem e uma string"
+// const s3 = "Isso e uma string tambem"
+
+// // number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//       document.write("executar uma grande logica de correr")    
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de codigo
+
+// regitrar funcao
+// function sayMyName(name) {
+
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valesca")
+// sayMyName("Douglas")
+
+// condicionais
+
+// const notafinal = 7
+
+// if( notafinal < 5 ) {
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repeticoes
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
---
+++ src/server.js
@@ -0,0 +1,106 @@
+const express = require("express")
+const server = express()
+
+// pegar o banco de dados
+const db = require("./database/db")
+
+// configurar pasta publica
+server.use(express.static("public"))
+
+// Habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos da minha aplicação
+// pagina inicial
+// req: Requisição
+// res: Resposta
+
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um título" })
+})
+server.get("/create-pet", (req, res) => {
+
+    // req.query: Query Strings da nossa url
+    // console.log(req.query)
+
+    return res.render("create-pet.html")
+})
+
+server.post("/savepoint", (req, res) => {
+
+    //  req.body: O corpo do nosso formulario
+    // console.log(req.body)
+
+    // inserir dados no banco de dados
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+
+    `
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastro com sucesso")
+        console.log(this)
+
+        return res.render("create-pet.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+
+})
+
+
+
+
+
+server.get("/search", (req, res) => {
+
+
+
+
+
+    // pegar dados do banco de dados
+    db.all(`SELECT * FROM pets`, function(err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        const total = rows.length
+            // mostrar a página html com os dados do banco de dados
+        return res.render("search-results.html", { pets: rows, total: total })
+    })
+
+
+})
+
+// Ligar o servidor
+server.listen(3000)
